cf698112864633bbb79de29ba81b2d54027d3655
Steve Lewis <stevelle@gmail.com>
2015-03-19 11:30:12 -0500

    Fixing install script issues
    Fixing git checkout path, and numerous paths resulting from that change
    Pre-creating the agent's logging directory

--- ext/install.sh
+++ ext/install.sh
@@ -7,20 +7,24 @@ apt-get -y install build-essential python python-dev python-virtualenv nginx sup
 # create a user to run the agent 
 adduser --disabled-password --gecos "" loadagent
 cd /home/loadagent
-git clone https://github.com/stevelle/loadagent.git .
+
+# fetch the agent source
+git clone https://github.com/stevelle/loadagent.git 
+cp loadagent/agent.py .
 
 # create a virtualenv and install dependencies
 virtualenv venv
-venv/bin/pip install -r requirements.txt
-chown -R loadagent:loadagent .
+venv/bin/pip install -r loadagent/requirements.txt
+chown -R loadagent:loadagent . 
 
 # configure supervisor to run a private gunicorn web server as a service
-cp ext/supervisor/loadagent.conf /etc/supervisor/conf.d/
+cp loadagent/ext/supervisor/loadagent.conf /etc/supervisor/conf.d/
+mkdir -p /var/log/loadagent
 supervisorctl reread
 supervisorctl update
 
 # enable reverse-proxy from nginx to the agent
-cp ext/nginx/loadagent /etc/nginx/sites-available/
+cp loadagent/ext/nginx/loadagent /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/loadagent /etc/nginx/sites-enabled/
 service nginx restart
 
